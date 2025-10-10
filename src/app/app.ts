f8731b18f0ee6518892f7329f79852513b371a2c
OMAR WAEL <omarwael012@gmail.com>
2025-10-10 05:09:55 +0300

    ADD : inttial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Landing } from './components/landing/landing';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , Landing , Header , Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/startquiz/startquiz.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-startquiz',
+  imports: [],
+  templateUrl: './startquiz.html',
+  styleUrl: './startquiz.css'
+})
+export class Startquiz {
+
+}
