60a25073ed9f5b6e732ee34348049409879a7bf5
HassanAli381 <alhassanali599@gmail.com>
2025-10-16 15:09:49 +0300

    Finish Grade Exam

--- src/app/components/grade-exam/grade-exam.ts
+++ src/app/components/grade-exam/grade-exam.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { GradeExamService } from '../../services/grade-exam-service';
+import { DatePipe, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-grade-exam',
-  imports: [],
+  imports: [NgClass, DatePipe, RouterLink],
   templateUrl: './grade-exam.html',
   styleUrl: './grade-exam.css'
 })
 export class GradeExam implements OnInit{
     questionsBody: any[] = [];
     apiUrl: string = '';
+    gradeData: any;
 
     constructor(private gradeExamService: GradeExamService, private router: Router) {
         const navigation = this.router.getCurrentNavigation();
@@ -26,6 +28,7 @@ export class GradeExam implements OnInit{
         this.gradeExamService.gradeExam(this.apiUrl, this.questionsBody).subscribe({
             next:(res) => {
                 console.log('res this.gradeExamService', res);
+                this.gradeData = res.data;
             },
             error: (err) => {
                 console.error('grade exam error', err);
