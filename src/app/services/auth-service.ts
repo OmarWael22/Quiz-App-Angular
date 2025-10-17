900033cedd6636c303394b12203c19acf1223c4e
HassanAli381 <alhassanali599@gmail.com>
2025-10-18 01:43:57 +0300

    Adding user _id to auth-service

--- src/app/services/auth-service.ts
+++ src/app/services/auth-service.ts
@@ -6,15 +6,17 @@ import { Injectable, signal } from '@angular/core';
 export class AuthService {
     isLoggedIn = signal(false); // to make CD in header
 
+    userID: string = '';
     userName: string = '';
     userEmail: string = '';
     userRole: string = '';
     userToken: string = '';
     
 
-    fillData(name: string, email: string, role: string, token: string) {
-        console.log('Filling the data', name, email, role, token);
+    fillData(id: string, name: string, email: string, role: string, token: string) {
+        console.log('Filling the data', id, name, email, role, token);
         this.isLoggedIn.set(true);
+        this.userID = id;
         this.userName = name;
         this.userEmail = email;
         this.userRole = role;
@@ -24,6 +26,7 @@ export class AuthService {
     clearData() {
         console.log('Clearing the data');
         this.isLoggedIn.set(false);
+        this.userID = '';
         this.userName = '';
         this.userEmail = '';
         this.userRole = '';
